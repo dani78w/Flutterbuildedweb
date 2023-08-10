abd07dc801b18e937eeffb40199789e70000b2e9
Daniel Arribas <93707372+dani78w@users.noreply.github.com>
2023-08-10 18:13:51 +0200

    update4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9e91839cf1f80fe0cc9726cff3befb33",
-"index.html": "a73f12eef4bfb087887aafcb5d2277a3",
-"/": "a73f12eef4bfb087887aafcb5d2277a3",
+"index.html": "2058e2afe80e45b2117f2150caacb2ff",
+"/": "2058e2afe80e45b2117f2150caacb2ff",
 "main.dart.js": "88f803c6f9bf52c80f9b08c1ef2bad82",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
